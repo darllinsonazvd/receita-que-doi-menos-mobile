75be701fc2c366ad25f7a23d7b8639e59bca5c8e
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-10-24 19:18:57 -0300

    feat(home): setup eslint and inital screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Text style={styles.text}>Receita Que Dói Menos!</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FECC01',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
   },
-});
+  text: {
+    color: '#932743',
+    fontWeight: '700',
+    fontSize: 32,
+  },
+})
