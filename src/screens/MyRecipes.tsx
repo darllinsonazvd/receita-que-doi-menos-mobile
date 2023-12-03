5ef19eab509989435405abf8d04830d47ad1d3d3
Pedro <pedroazevedoteixeiracontato@gmail.com>
2023-12-02 23:32:51 -0300

    My Recipes Routed

--- src/screens/MyRecipes.tsx
+++ src/screens/MyRecipes.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import {
@@ -15,15 +16,49 @@ import { RecipeCard } from '../components/RecipeCard'
 
 import Background from '../assets/img/bg-register.png'
 
-import { useState } from 'react';
+import { privateApi } from '../lib/api';
+
+import { JwtDecode } from '../utils/types/jwt'
+import * as SecureStore from 'expo-secure-store';
+import { SecureStoreKeys } from '../utils/enums/secure-store-keys';
+import { jwtDecode } from '../utils/functions/jwt-decode';
 
 type MyRecipesProps = {
   navigation: any
 }
 
 export default function MyRecipes({ navigation }: MyRecipesProps) {
-  const { top } = useSafeAreaInsets()
-  const [recipes] = useState<Recipe[]>([]);
+  const { top } = useSafeAreaInsets();
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [userInfo, setUserInfo] = useState<JwtDecode | null>(null);
+
+  const fetchRecipes = () => {
+    if (userInfo) {
+      const { user_id } = userInfo; // Acesso às propriedades do JWT
+  
+      privateApi
+        .get(`/user/createdRecipes/${user_id}`)
+        .then((response) => {
+          setRecipes(response.data);
+        })
+        .catch((error) => {
+          console.error('Erro ao buscar receitas:', error);
+        });
+    }
+  }
+  useEffect(() => {
+    /** Recuperando informações do usuário autenticado */
+    SecureStore.getItemAsync(SecureStoreKeys.TOKEN).then((token) => {
+      const decodedToken = jwtDecode(token || '')
+      setUserInfo(decodedToken)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (userInfo) {
+      fetchRecipes();
+    }
+  }, [userInfo]);
 
   return (
     <View className="flex-1 bg-zinc-100" style={{ paddingTop: top }}>
@@ -49,32 +84,27 @@ export default function MyRecipes({ navigation }: MyRecipesProps) {
 
             <View className="flex-1 flex-row items-center rounded-2xl bg-zinc-100 p-4 shadow-xl">
               <Text className="font-body text-base">
-                Fala Chef! Aqui são as receitas criadas por você, em breve você
-                poderá editá-las ou removê-las da nossa comunidade.
+                Fala Chef! Aqui são as receitas criadas por você, em breve você poderá editá-las ou removê-las da nossa comunidade.
               </Text>
             </View>
           </View>
 
-          {recipes.map((recipe) => {
-            return (
-              <TouchableOpacity
-                className="w-full"
-                activeOpacity={0.9}
-                key={recipe.id}
-                onPress={() =>
-                  navigation.navigate('RecipeDetails', { recipeId: recipe.id })
-                }
-              >
-                <RecipeCard
+          {recipes.map((recipe) => (
+            <TouchableOpacity
+              key={recipe.id}
+              onPress={() => navigation.navigate('RecipeDetails', { recipeId: recipe.id })}
+              className="w-full mb-4"
+            >
+              <Text className="text-lg font-semibold">{recipe.name}</Text>
+              <RecipeCard
                   key={recipe.id}
                   recipe={recipe}
                   showFavoriteButton={true}
-                />
-              </TouchableOpacity>
-            )
-          })}
+              />
+            </TouchableOpacity>
+          ))}
         </ImageBackground>
       </ScrollView>
     </View>
-  )
+  );
 }
